9a523f3dfbe3e672594c727d522624e2d08e9675
JyotinderSingh <jyotindrsingh@gmail.com>
2019-11-02 11:47:01 +0530

    Login Form

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
-import Link from 'react-router-dom/Link';
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 // MUI Stuff
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typorgraphy from '@material-ui/core/Typography';
-import Typography from '@material-ui/core/Typography';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 const styles = {
   card: {
-    display: 'flex',
+    display: "flex",
     marginBottom: 20
   },
   image: {
-    minWidth: 200,
+    minWidth: 200
   },
   content: {
     padding: 25,
-    objectFit: 'cover'
+    objectFit: "cover"
   }
 };
 
 class Scream extends Component {
   render() {
+      dayjs.extend(relativeTime)
     const {
       classes,
       scream: {
@@ -44,17 +46,17 @@ class Scream extends Component {
           title="Profile image"
           className={classes.image}
         />
-        <CardContent class={classes.content}>
-          <Typorgraphy
+        <CardContent className={classes.content}>
+          <Typography
             variant="h5"
             component={Link}
             to={`/users/${userHandle}`}
             color="primary"
           >
             {userHandle}
-          </Typorgraphy>
+          </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
 
-import Scream from '../components/Scream';
+import Scream from "../components/Scream";
 
 class home extends Component {
   state = {
@@ -10,18 +10,19 @@ class home extends Component {
   };
   componentDidMount() {
     axios
-      .get('/screams')
-      .then((res) => {
-        console.log(res.data);
+      .get("/screams")
+      .then(res => {
         this.setState({
           screams: res.data
         });
       })
-      .catch((err) => console.log(err));
+      .catch(err => console.log(err));
   }
   render() {
     let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map((scream) => <Scream scream={scream} />)
+      this.state.screams.map(scream => (
+        <Scream key={scream.screamId} scream={scream} />
+      ))
     ) : (
       <p>Loading...</p>
     );
