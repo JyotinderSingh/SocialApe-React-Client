06fd937d79fcbea30d8a32c3ec0b6114ab2e22bd
JyotinderSingh <jyotindrsingh@gmail.com>
2019-11-04 01:31:12 +0530

    Added Loading Skeleton

--- src/util/theme.js
+++ src/util/theme.js
@@ -1,53 +1,99 @@
 export default {
-    palette: {
-      primary: {
-        light: "#3585858",
-        main: "#363636",
-        dark: "#050505",
-        contrastText: "#fff"
-      },
-      secondary: {
-        light: "#62efff",
-        main: "#00b0c7",
-        dark: "#008ba3",
-        contrastText: "#fff"
+  palette: {
+    primary: {
+      light: "#3585858",
+      main: "#363636",
+      dark: "#050505",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#62efff",
+      main: "#00b0c7",
+      dark: "#008ba3",
+      contrastText: "#fff"
+    }
+  },
+  typography: {
+    useNextVariants: true
+  },
+  form: {
+    textAlign: "center"
+  },
+  image: {
+    margin: "20px auto 20px auto"
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto"
+  },
+  textField: {
+    margin: "10px auto 10px auto"
+  },
+  button: {
+    marginTop: 20,
+    position: "relative"
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.9rem",
+    marginTop: 15
+  },
+  progress: {
+    position: "absolute"
+  },
+  invisibleSeparator: {
+    border: "none",
+    margin: 4
+  },
+  visibleSeparator: {
+    width: "90%",
+    borderBottom: "1px solid rgba(0,0,0,0.1)",
+    marginBottom: 20,
+    marginTop: 10
+  },
+
+  paper: {
+    padding: 20
+  },
+  profile: {
+    "& .image-wrapper": {
+      textAlign: "center",
+      position: "relative",
+      "& button": {
+        position: "absolute",
+        top: "80%",
+        left: "70%"
       }
     },
-    typography: {
-      useNextVariants: true
+    "& .profile-image": {
+      width: 200,
+      height: 200,
+      objectFit: "cover",
+      maxWidth: "100%",
+      borderRadius: "50%"
     },
-    form: {
-      textAlign: "center"
+    "& .profile-details": {
+      textAlign: "center",
+      "& span, svg": {
+        verticalAlign: "middle"
+      },
+      "& a": {
+        color: "#00b0c7"
+      }
     },
-    image: {
-      margin: "20px auto 20px auto"
+    "& hr": {
+      border: "none",
+      margin: "0 0 10px 0"
     },
-    pageTitle: {
-      margin: "10px auto 10px auto"
-    },
-    textField: {
-      margin: "10px auto 10px auto"
-    },
-    button: {
-      marginTop: 20,
-      position: "relative"
-    },
-    customError: {
-      color: "red",
-      fontSize: "0.9rem",
-      marginTop: 15
-    },
-    progress: {
-      position: "absolute"
-    },
-    invisibleSeparator: {
-      border: 'none',
-      margin: 4
-    },
-    visibleSeparator: {
-      width: '90%',
-      borderBottom: '1px solid rgba(0,0,0,0.1)',
-      marginBottom: 20,
-      marginTop: 10
+    "& svg.button": {
+      "&:hover": {
+        cursor: "pointer"
+      }
+    }
+  },
+  buttons: {
+    textAlign: "center",
+    "& a": {
+      margin: "20px 10px"
     }
   }
+};
